833c00b1da5752373f64436bd69c90a6b1891242
AliMatroky <alimatroky@gmail.com>
2019-01-26 06:32:34 +0330

    inject in main method in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bitcoine/data/dependency_injection.dart';
 import 'package:flutter_bitcoine/home_page.dart';
 import 'package:flutter/foundation.dart';
 
-void main() => runApp(MyApp());
+void main() async { 
+  Injector.configure(Flaver.Prod);
+  runApp(MyApp());
+  }
 
 class MyApp extends StatelessWidget {
   @override
